23e7184815b8bc0f819a75130e8ef0e6b91fc295
fabrlyn <robert.barlin@gmail.com>
2021-01-16 08:16:11 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use select::document::Document;
+use select::predicate::Predicate;
+use select::predicate::{Attr, Name};
+
+fn main() {
+    let index = include_str!("./index.html");
+    let document = Document::from(index);
+    let head_line = document
+        .find(
+            Name("div")
+                .and(Attr("data-key", "main-story"))
+                .descendant(Name("a").and(Attr("data-key", "card-headline"))),
+        )
+        .into_selection()
+        .children()
+        .first()
+        .map(|n| n.text())
+        .map(|x| {
+            x.trim()
+                .split("\n")
+                .map(|s| s.trim())
+                .collect::<Vec<_>>()
+                .join(" ")
+        })
+        .unwrap_or_default();
+
+    println!("{}", head_line);
+}
